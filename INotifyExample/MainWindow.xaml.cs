1b0258025a82618a0469a536f3b87b2eb465d95f
Andy Myers <mail@jamyers.net>
2017-10-04 15:28:48 -0400

    Add project files.

---
+++ INotifyExample/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace INotifyExample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public Person person { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            person = new Person { FirstName = "Hi", LastName = "There" };
+            this.DataContext = person;
+        }
+    }
+}
---
+++ INotifyExample/Person.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace INotifyExample
+{
+    public class Person : INotifyPropertyChanged
+    {
+        private string firstName;
+        private string lastName;
+        private string fullName;
+
+        public string FirstName {
+            get { return firstName; }
+            set {
+                if(firstName != value)
+                {
+                    firstName = value;
+                    OnPropertyChanged("firstName");
+                }
+            }
+        }
+
+        public string LastName {
+            get { return lastName; }
+            set {
+                if(lastName != value)
+                {
+                    lastName = value;
+                    OnPropertyChanged("lastName");
+                }
+            }
+        }
+
+        public string FullName {
+            get { return fullName = firstName + " " + lastName; }
+            set {
+                if(fullName != value)
+                {
+                    fullName = value;
+                    OnPropertyChanged("fullName");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void  OnPropertyChanged(string property)
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+    }
+}
